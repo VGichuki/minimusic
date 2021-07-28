963f6a6951c0e778de2534c2430f89d46ba9d18e
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-28 14:01:08 +0300

    Create Music and album models

--- mini/models.py
+++ mini/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length=1000)
+    artist = models.CharField(max_length=500)
+    album = models.ForeignKey('Album',null=True, blank=True, on_delete=models.SET_NULL)
+    time_length = models.DecimalField(max_digits=50,decimal_places=2,blank=True)
+    audio_file = models.FileField(upload_to='musics')
+    image= models.ImageField(upload_to='music_image/')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.time_length:
+            #Login for getting music length
+            pass
+        return super().save(*args, **kwargs)
+
+class Album(models.Model):
+    name = models.CharField(max_length=1000)
+
+
+
+
