c6cdc4775bc7714fd414d1fc0f6a39ea5d33237b
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-28 12:59:03 +0300

    create a base.html to link all the other html

--- minimusic/settings.py
+++ minimusic/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mini.apps.MiniConfig',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
