36468eeee539a8dfdcde07c27d9872d27c9ae670
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-29 21:36:15 +0300

    create a logout url in the navbar

--- mini/urls.py
+++ mini/urls.py
@@ -9,6 +9,8 @@ urlpatterns=[
     path('new-music/', views.newMusic, name='new_music'),
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- mini/views.py
+++ mini/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
-from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -17,6 +17,7 @@ def home(request):
     music_list = list(Music.objects.all().values())
     return render(request, 'index.html', {'music': music, 'music_list': music_list})
 
+@login_required(login_url='login')
 def newMusic(request):
     form = MusicForm()
     if request.POST:
@@ -34,32 +35,41 @@ def newMusic(request):
     return render(request,'music.html',{'form': form})
 
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
-            return redirect('login')
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
     context={'form': form}
     return render(request, 'accounts/registration.html', context)
     
-@csrf_protect
 def loginPage(request):
-    if request.method == 'POST':
-        username =request.POST.get('username')
-        password =request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username =request.POST.get('username')
+            password =request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
-           
-    context={}
-    return render(request, 'accounts/login.html', context)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+            
+        context={}
+        return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
