a8208f4568fa30515f3d816331b9b90783f83671
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-29 17:07:00 +0300

    create a view function for the music.html

--- mini/views.py
+++ mini/views.py
@@ -1,11 +1,17 @@
+from mini.forms import MusicForm
 from mini.models import Music
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Music,Album
+from .forms import MusicForm
 
 # Create your views here.
 def home(request):
     music= Music.objects.all()
     music_list = list(Music.objects.all().values())
     return render(request, 'index.html', {'music': music, 'music_list': music_list})
+
+def newMusic(request):
+    form = MusicForm()
+    return render(request,'music.html',{'form': form})
 
